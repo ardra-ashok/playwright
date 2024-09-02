c1a9f00b4244208442cf8e921e0b40dc37ee7b52
ardra_ashok <ardraasok@yahoo.in>
2024-09-02 10:17:21 +1000

    Added radio button, check box and select input handling in playwright

--- tests/test_1.spec.js
+++ tests/test_1.spec.js
@@ -2,29 +2,26 @@ const { test, expect } = require('@playwright/test')
 const { log } = require('console')
 
 test('Test Login', async ({ browser }) => {
- const context = await browser.newContext()
- const page = await context.newPage()
- const userName = page.locator('#username')
- const signIn = page.locator('#signInBtn')
- const prodTitles = page.locator('.card-body a')
- await page.goto('https://rahulshettyacademy.com/loginpagePractise/')
- console.log(await page.title())
- 
- await userName.fill('rahulshetty')
- await page.locator('[type="password"]').fill("learning")
- await signIn.click();
- console.log(await page.locator("[style*='block']").textContent());
- await expect(page.locator("[style*='block']")).toContainText('Incorrect')
- await userName.fill('')
- await userName.fill('rahulshettyacademy')
- await signIn.click()
- console.log(await prodTitles.first().textContent())
- console.log(await prodTitles.nth(1).textContent())
- const allTitles = await prodTitles.allTextContents();
- console.log(allTitles);
- 
- 
- 
+  const context = await browser.newContext()
+  const page = await context.newPage()
+  const userName = page.locator('#username')
+  const signIn = page.locator('#signInBtn')
+  const prodTitles = page.locator('.card-body a')
+  await page.goto('https://rahulshettyacademy.com/loginpagePractise/')
+  console.log(await page.title())
+
+  await userName.fill('rahulshetty')
+  await page.locator('[type="password"]').fill('learning')
+  await signIn.click()
+  console.log(await page.locator("[style*='block']").textContent())
+  await expect(page.locator("[style*='block']")).toContainText('Incorrect')
+  await userName.fill('')
+  await userName.fill('rahulshettyacademy')
+  await signIn.click()
+  console.log(await prodTitles.first().textContent())
+  console.log(await prodTitles.nth(1).textContent())
+  const allTitles = await prodTitles.allTextContents()
+  console.log(allTitles)
 })
 
 test('Page playwright Test', async ({ page }) => {
@@ -34,47 +31,44 @@ test('Page playwright Test', async ({ page }) => {
   await expect(page).toHaveTitle('Google')
 })
 
-test.only('Test Login - Excercise', async ({ page }) => {
- // const context = await browser.newContext()
- // const page = await context.newPage()
+test('Test Login - Excercise', async ({ page }) => {
+  // const context = await browser.newContext()
+  // const page = await context.newPage()
 
- const firstName = page.locator('#firstName')
- const lastName = page.locator('#lastName')
- const email = page.locator('#userEmail')
- const registerPageBtn = page.locator('.login-wrapper-footer-text')
- const phone = page.locator('#userMobile')
- const password = page.locator('#userPassword')
- const confirmPassword = page.locator('#confirmPassword')
- const loginBtn = page.locator('#login')
- const ageConfirm = page.locator('[type="checkbox"]')
- const emailValue = "test_6@automation.com"
- const pw = 'testPw!123'
- const cardTitle = page.locator('.card-body h5 b')
+  const firstName = page.locator('#firstName')
+  const lastName = page.locator('#lastName')
+  const email = page.locator('#userEmail')
+  const registerPageBtn = page.locator('.login-wrapper-footer-text')
+  const phone = page.locator('#userMobile')
+  const password = page.locator('#userPassword')
+  const confirmPassword = page.locator('#confirmPassword')
+  const loginBtn = page.locator('#login')
+  const ageConfirm = page.locator('[type="checkbox"]')
+  const emailValue = 'test_6@automation.com'
+  const pw = 'testPw!123'
+  const cardTitle = page.locator('.card-body h5 b')
 
-// Register
- await page.goto('https://rahulshettyacademy.com/client')
- await registerPageBtn.click()
- await firstName.fill('testFName')
- await lastName.fill('testLName')
- await email.fill(emailValue)
- await phone.fill('1234567890')
- await password.fill(pw)
- await confirmPassword.fill(pw)
- await ageConfirm.check()
- await loginBtn.click();
+  // Register
+  await page.goto('https://rahulshettyacademy.com/client')
+  await registerPageBtn.click()
+  await firstName.fill('testFName')
+  await lastName.fill('testLName')
+  await email.fill(emailValue)
+  await phone.fill('1234567890')
+  await password.fill(pw)
+  await confirmPassword.fill(pw)
+  await ageConfirm.check()
+  await loginBtn.click()
 
- // login
- await page.goto('https://rahulshettyacademy.com/client')
- await email.fill(emailValue)
- await password.fill(pw)
- await loginBtn.click()
+  // login
+  await page.goto('https://rahulshettyacademy.com/client')
+  await email.fill(emailValue)
+  await password.fill(pw)
+  await loginBtn.click()
 
- await cardTitle.first().waitFor();
- console.log(await cardTitle.first().textContent())
+  await cardTitle.first().waitFor()
+  console.log(await cardTitle.first().textContent())
+  // await page.waitForLoadState('networkidle') // discouraged
 
- // await page.waitForLoadState('networkidle') // discouraged
- 
- console.log(await cardTitle.allTextContents())
- 
-
+  console.log(await cardTitle.allTextContents())
 })
---
+++ tests/test_2.spec.js
@@ -0,0 +1,32 @@
+const { test, expect } = require('@playwright/test')
+const { log } = require('console')
+
+
+test.only('Test Login - Excercise', async ({ page }) => {
+ await page.goto('https://rahulshettyacademy.com/loginpagePractise/')
+ const userName = page.locator('#username')
+ const signIn = page.locator('#signInBtn')
+ const dropDown = page.locator('select.form-control')
+ const documentLink = page.locator("[href*='documents-request']")
+
+ await dropDown.selectOption('consult')
+ await page.locator('.radiotextsty').last().click()
+ await page.locator('#okayBtn').click()
+
+ 
+ await expect(page.locator('.radiotextsty').last()).toBeChecked()
+ // another method 
+ // console.log(await page.locator('.radiotextsty').last().isChecked())
+
+ await page.locator('#terms').click()
+ await expect(page.locator('#terms')).toBeChecked()
+ await page.locator('#terms').uncheck()
+ expect(await page.locator('#terms').isChecked()).toBeFalsy()
+ await expect(documentLink).toHaveAttribute("class","blinkingText")
+
+
+ // await page.pause()
+
+ 
+
+})
