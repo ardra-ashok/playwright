d069bc169859d873e2dd103a4a14baedcb0579b9
ardra_ashok <ardraasok@yahoo.in>
2024-09-03 23:48:28 +1000

    finished with checkout page - adding inputs

--- tests/demo_app.spec.js
+++ tests/demo_app.spec.js
@@ -1,4 +1,5 @@
 const { test, expect } = require('@playwright/test')
+const { log } = require('console')
 
 test.only('Test Application', async ({ page }) => {
   const productName = 'IPHONE 13 PRO'
@@ -17,9 +18,37 @@ test.only('Test Application', async ({ page }) => {
       break
     }
   }
-  await page.locator('[routerlink*="cart"]').click()
+ await page.locator('[routerlink*="cart"]').click()
+ 
+ // Cart - Page
   // await page.locator('h3:has-text("IPHONE 13 PRO")').isVisible()
   await page.locator('div li').first().waitFor()
-  expect(await page.locator(`h3:has-text("${productName}")`).isVisible()).toBeTruthy()
-  // await page.pause()
+  expect(
+    await page.locator(`h3:has-text("${productName}")`).isVisible()
+  ).toBeTruthy()
+ await page.locator('text=Checkout').click()
+
+ // Checkout Page
+ await page.locator('div:has-text("CVV Code ?")').locator('+ input').fill('621')
+ await page.locator('div:has-text("Name on Card")').locator('+ input').fill('Anshika Test')
+ // await page.locator('[placeholder*="Select Country"]').pressSequentially('ind')
+ await page.locator('[placeholder*="Country"]').type('ind')
+ const countryOptions = await page.locator('.ta-results')
+ await countryOptions.waitFor()
+ const optionsCount = await countryOptions.locator('button').count()
+ 
+ for (let i = 0; i < optionsCount; i++){
+  const country = await countryOptions.locator("button").nth(i).textContent()
+  if (country.trim() === "India") { 
+   await countryOptions.locator("button").nth(i).click()
+   break
+  }
+ }
+ await page.locator('a:has-text("PLACE ORDER")').click()
+
+ 
+
+  
+
+  await page.pause()
 })
