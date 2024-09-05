be35af3f17e6f4d36561352a752f31d4306d8d04
ardra_ashok <ardraasok@yahoo.in>
2024-09-05 22:06:56 +1000

    added calendar automation

---
+++ tests/calender.spec.js
@@ -0,0 +1,30 @@
+const { test, expect } = require('@playwright/test')
+const { log } = require('console')
+
+test.only('Csalender Validation', async ({ page }) => {
+  const monthNumber = 7
+  const date = '25'
+  const year = '1984'
+
+  await page.goto('https://rahulshettyacademy.com/seleniumPractise/#/offers')
+  await page.locator('.react-date-picker__inputGroup').click()
+  await page.locator('.react-calendar__navigation__label').click()
+  await page.locator('.react-calendar__navigation__label').click()
+  await page.locator('.react-calendar__navigation__label').click()
+  await page
+    .locator(
+      'div.react-calendar__navigation .react-calendar__navigation__arrow'
+    )
+    .first()
+    .click()
+  await page.getByRole('button', { name: '– 1990' }).click()
+  await page.getByText(year).click()
+  await page.waitForSelector('.react-calendar__year-view__months__month')
+  await page
+    .locator('.react-calendar__year-view__months__month')
+    .nth(monthNumber - 1)
+    .click()
+  await page.locator(
+    `xpath=//abbr[text()='${date}']`
+  ).nth(1).click()
+})
