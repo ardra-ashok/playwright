ee4a74f55ad7aa2e44225027d57cdb32ab7e3d93
ardra-ashok <ardraasok@yahoo.in>
2024-09-13 23:25:10 +1000

    data fetched from another file

--- tests/demo_app_PO.spec.js
+++ tests/demo_app_PO.spec.js
@@ -1,35 +1,33 @@
-const { test, expect } = require('@playwright/test')
-const { log } = require('console')
-const { POManager } = require('../pageObjects/POManager')
+const { test, expect } = require("@playwright/test");
+const { log } = require("console");
+const { POManager } = require("../pageObjects/POManager");
+const productDetails = JSON.parse(JSON.stringify(require("./utils/orderDetails.json")));
 
-test('Test Application', async ({ page }) => {
-  const username = 'piyaasok@gmail.com'
-  const password = 'Test!12345'
-  const productName = 'IPHONE 13 PRO'
-  const products = await page.locator('.card-body')
-  const poManager = new POManager(page)
+for (const data of productDetails) {
+  test(`Test Application - ${data.productName}`, async ({ page }) => {
+    const poManager = new POManager(page);
 
-  const loginPage = await poManager.getLoginPage()
-  await loginPage.goToLogin()
-  await loginPage.validLogin(username, password)
+    const loginPage = await poManager.getLoginPage();
+    await loginPage.goToLogin();
+    await loginPage.validLogin(data.username, data.password);
 
-  const dashboardPage = await poManager.getDashboardPage()
-  await dashboardPage.searchProduct(productName)
-  await dashboardPage.navigateToCart()
+    const dashboardPage = await poManager.getDashboardPage();
+    await dashboardPage.searchProduct(data.productName);
+    await dashboardPage.navigateToCart();
 
-  // Cart - Page
-  const cartPage = await poManager.getCartPage()
-  await cartPage.verifyProductIsDisplayed(productName)
-  await cartPage.navigateToCheckOut()
-  // CheckoutPage
-  const checkOutPage = await poManager.getCheckOutPage()
-  await checkOutPage.enterDetailsAndPlaceOrder('India', username)
+    // Cart - Page
+    const cartPage = await poManager.getCartPage();
+    await cartPage.verifyProductIsDisplayed(data.productName);
+    await cartPage.navigateToCheckOut();
+    // CheckoutPage
+    const checkOutPage = await poManager.getCheckOutPage();
+    await checkOutPage.enterDetailsAndPlaceOrder("India", data.username);
 
-  const orderSummaryPage = await poManager.getOrderSummaryPage()
-  const orderId = await orderSummaryPage.confirmOrderAndGetOrderId()
-  await orderSummaryPage.navigateToOrdersPage()
+    const orderSummaryPage = await poManager.getOrderSummaryPage();
+    const orderId = await orderSummaryPage.confirmOrderAndGetOrderId();
+    await orderSummaryPage.navigateToOrdersPage();
 
-  const ordersPage = await poManager.getOrdersPage()
-  await ordersPage.verifyOrder(orderId)
-})
-
+    const ordersPage = await poManager.getOrdersPage();
+    await ordersPage.verifyOrder(orderId);
+  });
+}
