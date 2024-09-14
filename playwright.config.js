a787853c75553a5687fcf1f21ffd82c942093770
Ardra Ashok <ardraasok@yahoo.in>
2024-09-15 01:16:30 +1000

    generated allure reporting from allure results

--- playwright.config.js
+++ playwright.config.js
@@ -2,19 +2,20 @@
 const { defineConfig, devices } = require('@playwright/test');
 
 module.exports = defineConfig({
-  testDir: "./tests/",
+  testDir: './tests/',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
+  reporter: [['list'], ['allure-playwright']],
   // reporter: 'html',
-  reporter: "allure-playwright",
+  // reporter: "allure-playwright",
 
   use: {
-    browserName: "chromium",
+    browserName: 'chromium',
     headless: false,
-    screenshot: "on",
-    trace: "on",
+    screenshot: 'on',
+    trace: 'on',
     // trace: 'on-first-retry',
   },
   timeout: 30 * 1000,
@@ -38,4 +39,4 @@ module.exports = defineConfig({
   //     name: 'webkit',
   //     use: { ...devices['Desktop Safari'] },
   //   }]
-});
+})
