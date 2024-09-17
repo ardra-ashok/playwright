51d8a3ffe8c271bdfdd172d02315bee07a1dcd5f
ardra-ashok <ardraasok@yahoo.in>
2024-09-17 22:34:41 +1000

    added code to step definitions

--- features/ecommerce.feature
+++ features/ecommerce.feature
@@ -2,7 +2,7 @@ Feature: Ecommerce Validations
     
     Scenario: Placing the order
         Given I login to Ecommerce application using "piyaasok@gmail.com" and "Test123451!"
-        When I add "zara coat 3" to Cart
-        Then I verify "zara coat 3" is displayed in the Cart
+        When I add "IPHONE 13 PRO" to Cart
+        Then I verify "IPHONE 13 PRO" is displayed in the Cart
         When I enter valid details and place the order
         Then I verify the order is present in the order history
--- features/stepdefs/steps.js
+++ features/stepdefs/steps.js
@@ -1,30 +1,42 @@
+const { When, Then, Given } = require("@cucumber/cucumber");
+const playwright = require("@playwright/test");
+const { POManager } = require("../../pageObjects/POManager");
 
-    Given('I login to Ecommerce application using {string} and {string}', function (string, string2) {
-    
+Given(
+  "I login to Ecommerce application using {string} and {string}",
+  {timeout:100*1000},async function (username, password) {
+    const browser = await playwright.chromium.launch();
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    this.poManager = new POManager(page);
+    const loginPage = this.poManager.getLoginPage();
+    await loginPage.goToLogin();
+    await loginPage.validLogin(username, password);
+  }
+);
 
-    });
+When("I add {string} to Cart", {timeout:100*1000}, async function (productName) {
+  this.dashboardPage = this.poManager.getDashboardPage();
+  await this.dashboardPage.searchProduct(productName);
+  await this.dashboardPage.navigateToCart();
+});
 
+Then("I verify {string} is displayed in the Cart", async function (productName) {
+  const cartPage = await this.poManager.getCartPage();
+  await cartPage.verifyProductIsDisplayed(productName);
+  await cartPage.navigateToCheckOut();
+});
 
-    When('I add {string} to Cart', function (string) {
-    
-    
- 
-    });
+When("I enter valid details and place the order", async function () {
+  const checkOutPage = await this.poManager.getCheckOutPage();
+  await checkOutPage.enterDetailsAndPlaceOrder("India","piyaasok@gmail.com");
 
+  const orderSummaryPage = this.poManager.getOrderSummaryPage();
+  const orderId = await orderSummaryPage.confirmOrderAndGetOrderId();
+  await orderSummaryPage.navigateToOrdersPage();
+});
 
-    Then('I verify {string} is displayed in the Cart', function (string) {
-   
-   
- 
-    });
-
-    When('I enter valid details and place the order', function () {
-    
-    
- 
-    });
-
-    Then('I verify the order is present in the order history', function () {
-    
- 
-    });
+Then("I verify the order is present in the order history", async function () {
+  const ordersPage = this.poManager.getOrdersPage();
+  await ordersPage.verifyOrder(orderId);
+});
